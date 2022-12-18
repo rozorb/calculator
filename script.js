14ed565659819d3396738722eba33fd78985752e
Ron <ronnierozo@yahoo.com>
2022-12-18 15:59:15 -0500

    added listeners, and made changes to display

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+let chosenOperator = '';
+let previousDisplay = '';
+let currentDisplay = '';
+
+let clear = document.querySelector('#clear');
+let equal = document.querySelector('#equal');
+let decimal = document.querySelector('#decimal');
+
+let numbers = document.querySelectorAll('.number');
+let operators = document.querySelectorAll('.operator');
+
+let prev = document.querySelector('#prev');
+let curr = document.querySelector('#curr');
+
+numbers.forEach(number => number.addEventListener('click', function(e) {
+    getNumber(e.target.textContent);
+    curr.textContent = currentDisplay;
+}));
+
+operators.forEach(operator => operator.addEventListener('click', function(e) {
+    console.log(e.target.textContent);
+}));
+
+clear.addEventListener("click", function (){
+    currentDisplay = '';
+    previousDisplay = '';
+    operate = '';
+    curr.textContent = currentDisplay;
+    prev.textContent = previousDisplay;
+});
+
+function getNumber (num) {
+    if (currentDisplay.length <= 5) {
+        currentDisplay += num;
+    }  
+}
+
+
+
+
+
+
+
+
+
 function add(a,b) {
     console.log('inside add function '+ a + ' ' + b );
     return a + b;
@@ -27,5 +72,3 @@ function operate(operator, a, b) {
         return divide (a,b);
     }
 }
-
-console.log(operate('divide', 5, 8));
